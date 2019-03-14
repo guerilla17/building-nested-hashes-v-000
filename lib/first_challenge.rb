5668576788a9ccd86c8895a0ad19d52bb0b170d8
Gigi M <mmgamao1@gmail.com>
2019-03-14 19:54:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,16 @@
 def first_challenge
    epic_tragedy = {
      :Montague => {
-       :patriach,
-       :matriarch,
-       :hero,
-       :hero_friends
+       :patriach => {name: "Lord Montague", age: "53"},
+       :matriarch => {name: "Lady Montague", age: "54"},
+       :hero => {name: "Romeo", age: "15", status: "alive"},
+       :hero_friends => []
      },
      :Capulet => {
-       :patriach,
-       :matriarch,
-       :heroine,
-       :heroine_friends
+       :patriach => {name: "Lord Capulet", age: "50"},
+       :matriarch => {"Lady Capulet", age: "51"},
+       :heroine => {name: "Juliet", age: "15", status: "alive"},
+       :heroine_friends => []
      }
   
   
